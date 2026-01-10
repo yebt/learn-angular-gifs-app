50ddc335e05927b6ac158de568c6c44f706ed965
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-10 10:55:49 -0500

    feat(s6-c7): add updates

--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideMenuHeaderComponent } from '../../components/side-menu-header/side-menu-header.component';
-import { SideMenuOptionsComponent } from '../../components/side-menu-options/side-menu-options.component';
-import { SideMenuComponent } from "../../components/side-menu/side-menu.component";
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component({
   selector: 'gifs-dashboard-page',
