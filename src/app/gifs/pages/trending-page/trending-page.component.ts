9ba7e47ffcebbca0dec86a7f2b5c83869ecad6b5
Yahir <yahir.eduardo.bravo@gmail.com>
2026-01-14 00:53:56 -0500

    feat(s8-c7): add updates, detect is bottom

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.services';
 
+const SCROLL_INF_GAP = 300;
 
 @Component({
   selector: 'app-trending-page',
@@ -8,16 +9,26 @@ import { GifService } from '../../services/gifs.services';
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
-  gifService = inject(GifService)
+  gifService = inject(GifService);
 
-  scrollDivRef = viewChild<ElementRef>('groupDiv')
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
 
-  onScroll(event: Event){
+  onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
 
-    if(!scrollDiv){
-      return
-    }
+    if (!scrollDiv) return;
 
+    // scroll desde la parte de arriba, al inicio es 0
+    const scrollTop = scrollDiv.scrollTop;
+    // size of the viewposrt in hgeight
+    const clientHeight = scrollDiv.clientHeight;
+    // Size of the total element overflowed
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    const isAtBottom = scrollTop + clientHeight + SCROLL_INF_GAP >= scrollHeight;
+
+    if (isAtBottom){
+
+    }
   }
 }
