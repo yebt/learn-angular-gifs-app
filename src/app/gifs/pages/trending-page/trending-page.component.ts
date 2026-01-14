2192c73a2968e75bfccdf5079bb33424a9a908a6
Yahir <yahir.eduardo.bravo@gmail.com>
2026-01-14 00:46:51 -0500

    feat(s8-c6): add updates allow lisen scroll action

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,13 +1,23 @@
-import { Component, inject, signal } from '@angular/core';
-import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import { Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.services';
 
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GifListComponent],
+  imports: [],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
   gifService = inject(GifService)
+
+  scrollDivRef = viewChild<ElementRef>('groupDiv')
+
+  onScroll(event: Event){
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+
+    if(!scrollDiv){
+      return
+    }
+
+  }
 }
