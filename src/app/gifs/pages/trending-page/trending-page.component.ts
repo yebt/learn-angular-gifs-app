075fa0426bef0d3c0dccc38bacc80a3ae647aa7e
Yahir <yahir.eduardo.bravo@gmail.com>
2026-01-14 01:21:59 -0500

    feat(s8-c9): add updates, add scroll restore service to restore scroll in some page

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, inject, signal, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { GifService } from '../../services/gifs.services';
+import { ScrollStateService } from 'src/app/shared/service/scroll-state.service';
 
 const SCROLL_INF_GAP = 300;
 
@@ -8,10 +9,18 @@ const SCROLL_INF_GAP = 300;
   imports: [],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService);
 
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  // NOTE: restore scroll
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
 
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
@@ -26,9 +35,11 @@ export default class TrendingPageComponent {
     const scrollHeight = scrollDiv.scrollHeight;
 
     const isAtBottom = scrollTop + clientHeight + SCROLL_INF_GAP >= scrollHeight;
+    // WARNING: This is a bad practice, is better make a set
+    this.scrollStateService.trendingScrollState.set(scrollTop);
 
-    if (isAtBottom){
-      this.gifService.loadTrendingGifs()
+    if (isAtBottom) {
+      this.gifService.loadTrendingGifs();
     }
   }
 }
